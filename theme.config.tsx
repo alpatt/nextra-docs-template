58ed379f0efe657dfb90b60c0c9ee7774c9198d5
online-notary <amy@mediadrops.net>
2022-12-12 18:51:17 -0600

    updates

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,15 +2,8 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Collaborate</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
+  logo: <span>Dev Team Wiki</span>,
+     footer: {
     text: 'Nextra Docs Template',
   },
 }
